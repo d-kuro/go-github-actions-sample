a9da4f1e3126c0d156be557e52ebd72cdb52abc3
d-kuro <kurosawa7620@gmail.com>
2018-12-29 13:55:31 +0900

    Add docker file.

---
+++ .github/actions/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -e
+
+APP_DIR="/go/src/github.com/${GITHUB_REPOSITORY}/"
+
+mkdir -p ${APP_DIR} && cp -r ./ ${APP_DIR} && cd ${APP_DIR}
+
+export GO111MODULE=on
+go mod tidy
+go mod verify
+
+if [[ "$1" == "lint" ]]; then
+    echo "# Running GolangCI-Lint"
+    golangci-lint run --enable-all --disable gochecknoglobals --disable gochecknoinits
+fi
+
+if [[ "$1" == "test" ]]; then   
+    echo "# Running Test"
+    go test ./... -race
+fi
