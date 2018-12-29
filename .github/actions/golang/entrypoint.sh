4bb72bf736263c4d9eccf1d02a67ac167a72d66b
d-kuro <kurosawa7620@gmail.com>
2018-12-29 14:25:10 +0900

    WIP

--- .github/actions/golang/entrypoint.sh
+++ .github/actions/golang/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+echo "######################"
+
 APP_DIR="/go/src/github.com/${GITHUB_REPOSITORY}/"
 
 mkdir -p ${APP_DIR} && cp -r ./ ${APP_DIR} && cd ${APP_DIR}
 
+echo "######################"
+
 export GO111MODULE=on
 go mod tidy
 go mod verify
+
+echo "######################"
 
 if [[ "$1" == "lint" ]]; then
     echo "# Running GolangCI-Lint"
