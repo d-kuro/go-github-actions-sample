fee6fb4c0167d246b1361238c8759af73e7b6275
d-kuro <kurosawa7620@gmail.com>
2018-12-29 14:18:19 +0900

    WIP

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -1,15 +1,8 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-set -o pipefail
-set -e
+#!/bin/bash
 
 APP_DIR="/go/src/github.com/${GITHUB_REPOSITORY}/"
 
 mkdir -p ${APP_DIR} && cp -r ./ ${APP_DIR} && cd ${APP_DIR}
-
-pwd
 
 export GO111MODULE=on
 go mod tidy
