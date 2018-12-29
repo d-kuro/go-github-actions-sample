17aa19dc9caf6519039d9f517f51587a705b02c5
d-kuro <kurosawa7620@gmail.com>
2018-12-29 14:34:00 +0900

    Add echo.

--- .github/actions/golang/entrypoint.sh
+++ .github/actions/golang/entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-echo "######################"
-
 APP_DIR="/go/src/github.com/${GITHUB_REPOSITORY}/"
 
 mkdir -p ${APP_DIR} && cp -r ./ ${APP_DIR} && cd ${APP_DIR}
-
-echo "######################"
 
 export GO111MODULE=on
 go mod tidy
 go mod verify
 
-echo "######################"
-
 if [[ "$1" == "lint" ]]; then
+    echo "#######################"
     echo "# Running GolangCI-Lint"
     golangci-lint --version
     golangci-lint run --enable-all --disable gochecknoglobals --disable gochecknoinits
 fi
 
-if [[ "$1" == "test" ]]; then   
+if [[ "$1" == "test" ]]; then
+    echo "#######################"
     echo "# Running Test"
     go test ./... -race
 fi
